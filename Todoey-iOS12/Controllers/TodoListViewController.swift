0b6fafde0c6915f42a069156bc26575d0e231345
Mikhail Bhuta <mbhuta@usc.edu>
2018-10-10 17:16:41 +0530

    Implemented 'Codable' functionality for persistent storage

--- Todoey-iOS12/Controllers/TodoListViewController.swift
+++ Todoey-iOS12/Controllers/TodoListViewController.swift
@@ -10,19 +10,14 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    var itemArray = [String]()
-    
-    let defaults = UserDefaults.standard
+    var itemArray = [Item]()
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            
-            itemArray = items
-            
-        }
+        loadItems()
         
     }
     
@@ -36,7 +31,8 @@ class TodoListViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "todoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        cell.accessoryType = itemArray[indexPath.row].done ? .checkmark : .none
         
         return cell
         
@@ -46,12 +42,9 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        saveItems()
         
         tableView.deselectRow(at: indexPath, animated: true)
         
@@ -66,9 +59,11 @@ class TodoListViewController: UITableViewController {
             
             if let textField = alert.textFields, textField[0].text != "" {
                 
-                self.itemArray.append(textField[0].text!)
-                self.defaults.set(self.itemArray, forKey: "TodoListArray")
-                self.tableView.reloadData()
+                let newItem = Item()
+                newItem.title = textField[0].text!
+                self.itemArray.append(newItem)
+                
+                self.saveItems()
             }
             
         }
@@ -81,6 +76,37 @@ class TodoListViewController: UITableViewController {
         }
         alert.addAction(action)
         self.present(alert, animated: true, completion: nil)
+    }
+    
+    func saveItems() {
+        
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        }
+        catch {
+            print("Error encoding itemArray: \(error.localizedDescription)")
+        }
+        
+        self.tableView.reloadData()
+        
+    }
+    
+    func loadItems() {
+        
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            }
+            catch {
+                print(error.localizedDescription)
+            }
+            
+        }
     }
     
 }
---
+++ Todoey-iOS12/Models/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey-iOS12
+//
+//  Created by Mikhail Bhuta on 10/10/18.
+//  Copyright © 2018 MBhuta. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
